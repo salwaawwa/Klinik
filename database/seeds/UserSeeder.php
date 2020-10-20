82c25ebc20005015ff461198863505439597ba81
Aditya Nurdin <adityanurdin0@gmail.com>
2020-10-20 09:27:20 +0700

    fix error db:seed

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin2',
+            'email' => 'admin2@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'owner',
+            'status' => 1
+        ]);
+    }
+}
